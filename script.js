a32e38bf24456c567392b99151d30bd1bfe32dfe
poorvi-hub <poorvishrivastava17@gmail.com>
2024-08-13 04:29:36 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed("#element", {
+    strings: ["Web Developer", "Video Editor", "Graphic Designer"],
+    typeSpeed: 50,
+  });
+  
+  function downloadResume() {
+    window.open('path/to/your/resume.pdf', '_blank');
+  }
+  
+  function visitGithub() {
+    window.open('https://github.com/yourusername', '_blank');
+  }
+  
